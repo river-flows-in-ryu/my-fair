3ed602cd91f24cb31ad1efefae46185b196b7df3
river-flows-in-ryu <rgr65066384@gmail.com>
2024-10-06 19:11:14 +0900

    HotFix - tailwind config - tailwind config theme 실수로 인한 날림 😅

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,15 +2,22 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}", // Note the addition of the `app` directory.
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
 
-    // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        pretendard: ["Pretendard", "sans-serif"],
+      },
+      boxShadow: {
+        custom:
+          "0px 16px 32px 0px rgba(0, 0, 0, 0.12), 0px 0px 6px 0px rgba(0, 0, 0, 0.06)",
+      },
+    },
   },
   plugins: [],
 };
